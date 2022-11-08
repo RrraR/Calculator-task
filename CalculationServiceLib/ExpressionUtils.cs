92ffb8629c837a6ab4c1cb3facf7d8156687c714
RrraR <tanja.budnitskay@gmail.com>
2022-11-08 23:27:05 +0100

    fixes

--- CalculationServiceLib/ExpressionUtils.cs
+++ CalculationServiceLib/ExpressionUtils.cs
@@ -23,12 +23,12 @@ public static class ExpressionUtils
                 {
                     j++;
                     Array.Resize(ref result, j + 1);
-                    if (i == expression.Length - 1 || expression[i + 1] != '.')
+                    if (i == expression.Length - 1)
                     {
                         result[j] += expression[i].ToString();
                         i++;
                     }
-                    else if (expression[i + 1] == '.')
+                    else if (expression[i + 1] == '.' || char.IsNumber(expression[i + 1]))
                     {
                         var k = i;
                         var temp = "";
