a19b7ea4cb266346a67dc842908d2561cb77f6a8
RrraR <tanja.budnitskay@gmail.com>
2022-11-10 12:19:02 +0100

    fixes

--- CalculationServiceLib/ExpressionUtils.cs
+++ CalculationServiceLib/ExpressionUtils.cs
@@ -9,8 +9,7 @@ public static class ExpressionUtils
         var i = 0;
         while (i < expression.Length)
         {
-
-            if (!double.TryParse(expression[i].ToString(), out _))
+            if (!char.IsNumber(expression[i]))
             {
                 j++;
                 Array.Resize(ref result, j + 1);
@@ -19,16 +18,17 @@ public static class ExpressionUtils
             }
             else
             {
-                if (i == 0 || !double.TryParse(expression[i - 1].ToString(), out _))
+                if (i == 0 || !char.IsNumber(expression[i - 1]))
                 {
                     j++;
                     Array.Resize(ref result, j + 1);
-                    if (i == expression.Length - 1)
+                    if (i == expression.Length - 1 && char.IsNumber(expression[^1]))
                     {
                         result[j] += expression[i].ToString();
                         i++;
                     }
-                    else if (expression[i + 1] == '.' || char.IsNumber(expression[i + 1]) || char.IsNumber(expression[i]))
+                    else if (expression[i + 1] == '.' || char.IsNumber(expression[i + 1]) ||
+                             char.IsNumber(expression[i]))
                     {
                         var k = i;
                         var temp = "";
